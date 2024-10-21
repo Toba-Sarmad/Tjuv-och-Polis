f929278289aeffbbd5c149d133b5bd453234d21e
Toba-Sarmad <sarmadtoba@gmail.com>
2024-10-21 14:01:47 +0200

    Tar bort kommatecknet efter spelplanWidth i (Persons Police), råkat hamna däär

--- Tjuv och Polis/Program.cs
+++ Tjuv och Polis/Program.cs
@@ -7,17 +7,33 @@
             int spelplanWidth = 100;
             int speplanHeight = 25; 
 
-            List<Person> allPersons = new List<Person>();
+            List<Persons> allPersons = new List<Persons>();
 
             Random rnd = new Random();
 
-            //Skapa persons
-            //Tjuv
+            //Skapa persons Police
+            
             for(int i = 0; i < 10; i++)
             {
-                int startx = rnd.Next(1, spelplanWidth, -1);
-                int starty = rnd.Next(1, speplanHeight, -1);
-                allPersons.Add(new Police(startx))
+                int startx = rnd.Next(1, spelplanWidth -1);
+                int starty = rnd.Next(1, speplanHeight -1);
+                allPersons.Add(new Police(startx, starty));
+            }
+
+            //Skapa persons Tjuvar
+            for(int i = 0; i < 20; i++)
+            {
+                int startx = rnd.Next(1, spelplanWidth - 1);
+                int starty = rnd.Next(1, speplanHeight - 1);
+                allPersons.Add(new Theif(startx, starty));
+            }
+
+            //Skapa persons Citizen
+            for(int i = 0; i < 30; i++)
+            {
+                int startx = rnd.Next(1, spelplanWidth - 1);
+                int starty = rnd.Next(1, speplanHeight - 1);
+                allPersons.Add(new Citizen(startx, starty));
             }
         }
     }
